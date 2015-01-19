c7b528bde5affab942d5154ce3c0880c8bf8ab7d
wescravens <wesley.r.cravens@gmail.com>
2015-01-19 15:29:22 -0600

    added postinstall.sh to check if bower exists before trying to install

--- Gruntfile.js
+++ Gruntfile.js
@@ -373,6 +373,7 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             'package.json',
+            'postinstall.sh',
             'server/**/*'
           ]
         }]
